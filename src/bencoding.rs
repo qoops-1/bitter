79d7d6748f862e9ca59338cd02c92a8589691df9
Ilya Gladyshev <ilya.v.gladyshev@gmail.com>
2023-07-02 11:37:43 +0300

    str/int decoders done

--- src/bencoding.rs
+++ src/bencoding.rs
@@ -1,6 +1,7 @@
-use std::{collections::HashMap, num::ParseIntError, str::FromStr};
+use std::{collections::HashMap, num::ParseIntError, io::{Cursor, SeekFrom, Seek}};
 
-enum BencodedValue {
+#[derive(Debug, PartialEq, Eq)]
+pub enum BencodedValue {
     BencodedInt(i64),
     BencodedStr(String),
     BencodedList(Box<[BencodedValue]>),
@@ -12,61 +13,93 @@ pub trait BDecode: Sized {
 
 // Some standard type implementations
 
-fn bdecode_int<T: FromStr<Err = ParseIntError>>(s: &str) -> Result<T, String> {
-    let ascii_bytes: &[u8] = s.as_bytes();
-    if ascii_bytes.len() == 0 || ascii_bytes[0] != b'i' {
+pub fn bdecode_any(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
+    bdecode_int(s)
+        .or(bdecode_str(s))
+        .or(bdecode_dict(s))
+        .or(bdecode_list(s))
+}
+
+pub fn bdecode_int(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
+    let ascii_bytes = s.get_ref().as_bytes();
+    let cur_pos = s.stream_position().map_err(|e| e.to_string())? as usize;
+    if cur_pos == ascii_bytes.len() || ascii_bytes[cur_pos] != b'i' {
         return Result::Err("not an integer".to_owned());
     }
-    let number_end = s.find('e').ok_or("end of integer not found")?;
-    s[1..number_end]
+    let number_end = ascii_bytes[cur_pos..].iter().position(|c| *c == b'e').ok_or("end of integer not found")?;
+    
+    s.seek(SeekFrom::Current(number_end as i64)).map_err(|e| e.to_string())?;
+    s.get_ref()[1..number_end]
         .parse()
         .map_err(|parse_err: ParseIntError| parse_err.to_string())
+        .map(BencodedValue::BencodedInt)
 }
 
-fn bdecode_dict(s: &str) -> Result<BencodedValue, String> {
-    let ascii_bytes: &[u8] = s.as_bytes();
-    if ascii_bytes.len() == 0 || ascii_bytes[0] != b'd' {
+pub fn bdecode_dict(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
+    let ascii_bytes = s.get_ref().as_bytes();
+    let cur_pos = s.stream_position().map_err(|e| e.to_string())? as usize;
+    if cur_pos == ascii_bytes.len() || ascii_bytes[cur_pos] != b'd' {
         return Result::Err("not a dict".to_owned());
     }
-    BencodedValue::bdecode(&s[1..])
+    s.seek(SeekFrom::Current(1));
+    bdecode_any(s);
+    unimplemented!();
 }
 
-impl BDecode for BencodedValue {
-    fn bdecode(s: &str) -> Result<Self, String> {}
-}
-impl BDecode for u64 {
-    fn bdecode(s: &str) -> Result<Self, String> {
-        bdecode_int(s)
-    }
+pub fn bdecode_list(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
+    unimplemented!()
 }
 
-impl BDecode for String {
-    fn bdecode(s: &str) -> Result<Self, String> {
-        let len_end = s.find(':').ok_or("length prefix not found".to_owned())?;
-        let strlen: usize = s[..len_end]
+pub fn bdecode_str(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
+        let ascii_bytes = s.get_ref().as_bytes();
+        let cur_pos = s.stream_position().map_err(|e| e.to_string())? as usize;
+        
+        let len_end = ascii_bytes[cur_pos..].iter().position(|c| *c == b':').ok_or("length prefix not found".to_owned())? + cur_pos;
+        let strlen: usize = s.get_ref()[cur_pos..len_end]
             .parse()
             .map_err(|parse_err: ParseIntError| parse_err.to_string())?;
 
-        Result::Ok(s[len_end + 1..len_end + strlen + 1].to_owned())
-    }
+        let str_end = len_end + strlen + 1;
+        if ascii_bytes.len() < str_end {
+            return Result::Err("string length too long".to_owned());
+        }
+        Result::Ok(BencodedValue::BencodedStr(s.get_ref()[len_end + 1..str_end].to_owned()))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn bdecode_i64() {
-        assert_eq!(0, i64::bdecode("i0e").unwrap());
-        assert_eq!(-1, i64::bdecode("i-1e").unwrap());
-        assert_eq!(342, i64::bdecode("i342e").unwrap())
+    fn bdecode_int_test() {
+        assert_eq!(BencodedValue::BencodedInt(0), bdecode_int(&mut Cursor::new("i0e")).unwrap());
+        assert_eq!(BencodedValue::BencodedInt(-1), bdecode_int(&mut Cursor::new("i-1e")).unwrap());
+        assert_eq!(BencodedValue::BencodedInt(342), bdecode_int(&mut Cursor::new("i342e")).unwrap())
     }
 
     #[test]
-    fn bdecode_i64_error() {
-        assert!(i64::bdecode("ie").is_err());
-        assert!(i64::bdecode("i342").is_err());
-        assert!(i64::bdecode("342e").is_err());
-        assert!(i64::bdecode("342").is_err());
-        assert!(i64::bdecode("").is_err());
+    fn bdecode_int_error_test() {
+        assert!(bdecode_int(&mut Cursor::new("ie")).is_err());
+        assert!(bdecode_int(&mut Cursor::new("i342")).is_err());
+        assert!(bdecode_int(&mut Cursor::new("342e")).is_err());
+        assert!(bdecode_int(&mut Cursor::new("342")).is_err());
+        assert!(bdecode_int(&mut Cursor::new("")).is_err());
+    }
+
+    #[test]
+    fn bdecode_str_test() {
+        assert_eq!(BencodedValue::BencodedStr("hello".to_owned()), bdecode_str(&mut Cursor::new("5:hello")).unwrap());
+        assert_eq!(BencodedValue::BencodedStr(String::new()), bdecode_str(&mut Cursor::new("0:")).unwrap());
+        assert_eq!(BencodedValue::BencodedStr("hello:friends".to_owned()), bdecode_str(&mut Cursor::new("13:hello:friends")).unwrap());
+        assert_eq!(BencodedValue::BencodedStr("1".to_owned()), bdecode_str(&mut Cursor::new("1:1")).unwrap());
+    }
+
+    #[test]
+    fn bdecode_str_error_test() {
+        assert!(bdecode_str(&mut Cursor::new("1:")).is_err());
+        assert!(bdecode_str(&mut Cursor::new("5:hell")).is_err());
+        assert!(bdecode_str(&mut Cursor::new("hello")).is_err());
+        assert!(bdecode_str(&mut Cursor::new("")).is_err());
+        assert!(bdecode_str(&mut Cursor::new(":")).is_err());
+        assert!(bdecode_str(&mut Cursor::new("hello:hello")).is_err());
     }
 }
