2e3d571e87fa3e8e239afed3472408b6e053953f
Ilya Gladyshev <ilya.v.gladyshev@gmail.com>
2023-07-06 18:34:35 +0300

    this ugly but can't do better

--- src/bencoding.rs
+++ src/bencoding.rs
@@ -7,24 +7,55 @@ pub enum BencodedValue {
     BencodedList(Box<[BencodedValue]>),
     BencodedDict(HashMap<String, BencodedValue>),
 }
+
+impl BencodedValue {
+    pub fn try_into_string(self) -> Result<String, String> {
+        match self {
+            BencodedValue::BencodedStr(s) => Ok(s),
+            _ => Err("not a string".to_owned()),
+        }
+    }
+
+    pub fn try_into_dict(self) -> Result<HashMap<String, BencodedValue>, String> {
+        match self {
+            BencodedValue::BencodedDict(d) => Ok(d),
+            _ => Err("not a dict".to_owned()),
+        }
+    }
+
+    pub fn try_into_int(self) -> Result<i64, String> {
+        match self {
+            BencodedValue::BencodedInt(i) => Ok(i),
+            _ => Err("not an int".to_owned()),
+        }
+    }
+
+    pub fn try_into_list(self) -> Result<Box<[BencodedValue]>, String> {
+        match self {
+            BencodedValue::BencodedList(l) => Ok(l),
+            _ => Err("not a list".to_owned()),
+        }
+    }
+}
+
 pub trait BDecode: Sized {
-    fn bdecode(s: &str) -> Result<Self, String>;
+    fn bdecode(s: &BencodedValue) -> Result<Self, String>;
 }
 
 // Some standard type implementations
 
 pub fn bdecode_any(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
-    bdecode_int(s)
-        .or(bdecode_str(s))
-        .or(bdecode_dict(s))
-        .or(bdecode_list(s))
+    bdecode_int(s).map(BencodedValue::BencodedInt)
+        .or(bdecode_str(s).map(BencodedValue::BencodedStr))
+        .or(bdecode_dict(s).map(BencodedValue::BencodedDict))
+        .or(bdecode_list(s).map(BencodedValue::BencodedList))
 }
 
-pub fn bdecode_int(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
+pub fn bdecode_int(s: &mut Cursor<&str>) -> Result<i64, String> {
     let ascii_bytes = s.get_ref().as_bytes();
     let cur_pos = s.stream_position().map_err(|e| e.to_string())? as usize;
     if cur_pos == ascii_bytes.len() || ascii_bytes[cur_pos] != b'i' {
-        return Result::Err("not an integer".to_owned());
+        return Err("not an integer".to_owned());
     }
     let number_end = ascii_bytes[cur_pos..].iter().position(|c| *c == b'e').ok_or("end of integer not found")?;
     
@@ -32,25 +63,32 @@ pub fn bdecode_int(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
     s.get_ref()[1..number_end]
         .parse()
         .map_err(|parse_err: ParseIntError| parse_err.to_string())
-        .map(BencodedValue::BencodedInt)
 }
 
-pub fn bdecode_dict(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
+pub fn bdecode_dict(s: &mut Cursor<&str>) -> Result<HashMap<String, BencodedValue>, String> {
+    let mut map: HashMap<String, BencodedValue> = HashMap::new();
     let ascii_bytes = s.get_ref().as_bytes();
     let cur_pos = s.stream_position().map_err(|e| e.to_string())? as usize;
     if cur_pos == ascii_bytes.len() || ascii_bytes[cur_pos] != b'd' {
-        return Result::Err("not a dict".to_owned());
+        return Err("not a dict".to_owned());
     }
-    s.seek(SeekFrom::Current(1));
-    bdecode_any(s);
-    unimplemented!();
+    s.seek(SeekFrom::Current(1)).map_err(|e| e.to_string())?;
+    while s.stream_position().unwrap() < s.get_ref().len() as u64 {
+        let key = bdecode_str(s)?;
+        let value = bdecode_any(s)?;
+        if map.contains_key(&key) {
+            return Err(format!("Duplicate key \"{}\" in dict", key));
+        }
+        map.insert(key, value);
+    }
+    Ok(map)
 }
 
-pub fn bdecode_list(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
+pub fn bdecode_list(s: &mut Cursor<&str>) -> Result<Box<[BencodedValue]>, String> {
     unimplemented!()
 }
 
-pub fn bdecode_str(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
+pub fn bdecode_str(s: &mut Cursor<&str>) -> Result<String, String> {
         let ascii_bytes = s.get_ref().as_bytes();
         let cur_pos = s.stream_position().map_err(|e| e.to_string())? as usize;
         
@@ -61,9 +99,9 @@ pub fn bdecode_str(s: &mut Cursor<&str>) -> Result<BencodedValue, String> {
 
         let str_end = len_end + strlen + 1;
         if ascii_bytes.len() < str_end {
-            return Result::Err("string length too long".to_owned());
+            return Err("string length too long".to_owned());
         }
-        Result::Ok(BencodedValue::BencodedStr(s.get_ref()[len_end + 1..str_end].to_owned()))
+        Ok(s.get_ref()[len_end + 1..str_end].to_owned())
 }
 
 #[cfg(test)]
@@ -71,9 +109,9 @@ mod tests {
     use super::*;
     #[test]
     fn bdecode_int_test() {
-        assert_eq!(BencodedValue::BencodedInt(0), bdecode_int(&mut Cursor::new("i0e")).unwrap());
-        assert_eq!(BencodedValue::BencodedInt(-1), bdecode_int(&mut Cursor::new("i-1e")).unwrap());
-        assert_eq!(BencodedValue::BencodedInt(342), bdecode_int(&mut Cursor::new("i342e")).unwrap())
+        assert_eq!(0, bdecode_int(&mut Cursor::new("i0e")).unwrap());
+        assert_eq!(-1, bdecode_int(&mut Cursor::new("i-1e")).unwrap());
+        assert_eq!(342, bdecode_int(&mut Cursor::new("i342e")).unwrap())
     }
 
     #[test]
@@ -87,10 +125,10 @@ mod tests {
 
     #[test]
     fn bdecode_str_test() {
-        assert_eq!(BencodedValue::BencodedStr("hello".to_owned()), bdecode_str(&mut Cursor::new("5:hello")).unwrap());
-        assert_eq!(BencodedValue::BencodedStr(String::new()), bdecode_str(&mut Cursor::new("0:")).unwrap());
-        assert_eq!(BencodedValue::BencodedStr("hello:friends".to_owned()), bdecode_str(&mut Cursor::new("13:hello:friends")).unwrap());
-        assert_eq!(BencodedValue::BencodedStr("1".to_owned()), bdecode_str(&mut Cursor::new("1:1")).unwrap());
+        assert_eq!("hello", bdecode_str(&mut Cursor::new("5:hello")).unwrap());
+        assert_eq!(String::new(), bdecode_str(&mut Cursor::new("0:")).unwrap());
+        assert_eq!("hello:friends", bdecode_str(&mut Cursor::new("13:hello:friends")).unwrap());
+        assert_eq!("1", bdecode_str(&mut Cursor::new("1:1")).unwrap());
     }
 
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod bencoding;
 
-use bencoding::BDecode;
+use std::io::Cursor;
+
+use bencoding::*;
 
 fn main() {
     println!("Hello, world!");
@@ -12,19 +14,46 @@ struct Metainfo {
     info: MetainfoInfo,
 }
 
+fn bdecode<T : BDecode>(s: &str) -> Result<T, String> {
+    let mut c = Cursor::new(s);
+    let bencoded = bdecode_any(&mut c)?;
+
+    T::bdecode(&bencoded)
+}
+
 impl BDecode for Metainfo {
-    fn bdecode(s: &str) -> Result<Self, String> {
-        unimplemented!()
+    fn bdecode(benc: &BencodedValue) -> Result<Self, String> {
+        let dict = benc.try_into_dict()?;
+        let announce = dict.get("announce").ok_or("Required field not found")?.try_into_string()?;
+        let info = dict.get("info").ok_or("Required field not found")?;
+
+        Ok(Metainfo {
+            announce,
+            info: MetainfoInfo::bdecode(info)?,
+        })
     }
 }
 
 #[derive(Debug)]
 struct MetainfoInfo {
     name: String,
-    piece_length: u64,
+    piece_length: i64,
     pieces: String,
     files: Box<[MetainfoFile]>,
     length: u64,
+}
+
+impl BDecode for MetainfoInfo {
+    fn bdecode(benc: &BencodedValue) -> Result<Self, String> {
+        let dict = benc.try_into_dict()?;
+        let name = dict.get("name").ok_or("Required field not found")?.try_into_string()?;
+        let piece_length = dict.get("piece_length").ok_or("Required field not found")?.try_into_int()?;
+
+        Ok(MetainfoInfo {
+            name,
+            info: MetainfoInfo::bdecode(info)?,
+        })
+    }
 }
 
 #[derive(Debug)]
